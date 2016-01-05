eb6f836d2c73254d5dd115922ec44ca7ea33b158
Brett Tau <brett.tau@gmail.com>
2016-01-05 11:59:30 -0600

    Initial Commit

---
+++ CoffeeFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeFinder
+//
+//  Created by Brett Tau on 1/5/16.
+//  Copyright (c) 2016 Brett Tau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeFinder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoffeeFinder
+//
+//  Created by Brett Tau on 1/5/16.
+//  Copyright (c) 2016 Brett Tau. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
